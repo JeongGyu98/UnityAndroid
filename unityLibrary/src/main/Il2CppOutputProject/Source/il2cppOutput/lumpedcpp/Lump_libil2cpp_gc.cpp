8601eea9fb4d34a64e09a12cdef68039c4ec975a
kim-jeonggyu <sosqre0105@naver.com>
2022-02-06 20:21:49 +0900

    Laucher

---
+++ unityLibrary/src/main/Il2CppOutputProject/Source/il2cppOutput/lumpedcpp/Lump_libil2cpp_gc.cpp
@@ -0,0 +1,7 @@
+#include "il2cpp-config.h"
+#include "/Users/kim-jeonggyu/AndroidStudioProjects/TEST2/unityLibrary/src/main/Il2CppOutputProject/IL2CPP/libil2cpp/gc/BoehmGC.cpp"
+#include "/Users/kim-jeonggyu/AndroidStudioProjects/TEST2/unityLibrary/src/main/Il2CppOutputProject/IL2CPP/libil2cpp/gc/GCHandle.cpp"
+#include "/Users/kim-jeonggyu/AndroidStudioProjects/TEST2/unityLibrary/src/main/Il2CppOutputProject/IL2CPP/libil2cpp/gc/GarbageCollector.cpp"
+#include "/Users/kim-jeonggyu/AndroidStudioProjects/TEST2/unityLibrary/src/main/Il2CppOutputProject/IL2CPP/libil2cpp/gc/NullGC.cpp"
+#include "/Users/kim-jeonggyu/AndroidStudioProjects/TEST2/unityLibrary/src/main/Il2CppOutputProject/IL2CPP/libil2cpp/gc/WriteBarrier.cpp"
+#include "/Users/kim-jeonggyu/AndroidStudioProjects/TEST2/unityLibrary/src/main/Il2CppOutputProject/IL2CPP/libil2cpp/gc/WriteBarrierValidation.cpp"
